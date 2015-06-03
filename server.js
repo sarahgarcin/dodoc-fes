c158fe0c81b86499646b2b76fa980b2136c0a400
gsarah <sarahgarcin@hotmail.com>
2015-06-03 17:25:30 +0200

    add https access + implementing powermate

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var app     = express();
 var http    = require("http").createServer(app);
-var io      = require("socket.io").listen(http);
+var https = require('https');
+var fs = require('fs');
+var privateKey  = fs.readFileSync('file.pem', 'utf8');
+var certificate = fs.readFileSync('file.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+var io      = require("socket.io").listen(httpsServer);
 // var readDir = require('readdir');
 
 var main    = require('./main');
@@ -25,6 +32,6 @@ router(app, io, m);
 /**
 * Start the http server at port and IP defined before
 */
-http.listen(app.get("port"), /*app.get("ipaddr"),*/ function() {
-  console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
+httpsServer.listen(app.get("port"), /*app.get("ipaddr"),*/ function() {
+  console.log("Server up and running. Go to https://" + app.get("ipaddr") + ":" + app.get("port"));
 });
