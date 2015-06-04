1fda18ac9dd49e1f5a14db230d0df51d6b89988c
gsarah <sarahgarcin@hotmail.com>
2015-06-04 14:54:02 +0200

    dynamic host url

--- public/js/flux.js
+++ public/js/flux.js
@@ -2,9 +2,10 @@ jQuery(document).ready(function($) {
 
 	var serverBaseUrl = document.domain;
 	var domainUrl = window.location.href;
+	var host = window.location.host;
 	var socket = io.connect();
 	var sessionId = '';
-
+	console.log(host);
 	/**
 	* Events
 	*/
@@ -37,39 +38,39 @@ jQuery(document).ready(function($) {
     	var extension = array[i].split('.').pop();
     	var identifiant =  array[i].replace("." + extension, "");
 			if(extension == "jpg"){
-				$('.container-flux .content ul').prepend("<li class='images-bibli' id='"+ identifiant+"'' ><img src='https://localhost:8080/" + app.session + "/" + array[i] + "'></li>");
+				$('.container-flux .content ul').prepend("<li class='images-bibli' id='"+ identifiant+"'' ><img src='https://"+host+"/" + app.session + "/" + array[i] + "'></li>");
 			}
 			if(extension == "webm" || extension == "mp4"){
-				$('.container-flux .content ul').prepend("<li class='videos-bibli' id='"+ identifiant+"'' ><video src='https://localhost:8080/" + app.session + "/" + array[i] + "' controls></li>");""
+				$('.container-flux .content ul').prepend("<li class='videos-bibli' id='"+ identifiant+"'' ><video src='https://"+host+"/" + app.session + "/" + array[i] + "' controls></li>");""
 			}
 			if(extension == "wav"){
-				$('.container-flux .content ul').prepend("<li class='sons-bibli' id='"+ identifiant+"'' ><audio src='https://localhost:8080/" + app.session + "/" + array[i] + "' controls></li>");""
+				$('.container-flux .content ul').prepend("<li class='sons-bibli' id='"+ identifiant+"'' ><audio src='https://"+host+"/" + app.session + "/" + array[i] + "' controls></li>");""
 			}
 		}
 	}
 
 	function displayNewImage(req){
 		if(req.extension == "jpg"){
-			$('.container-flux .content ul').prepend("<li class='images-bibli' id='"+ req.title+"'' ><img src='https://localhost:8080/" + app.session + "/" + req.file + "'></li>");
+			$('.container-flux .content ul').prepend("<li class='images-bibli' id='"+ req.title+"'' ><img src='https://"+host+"/" + app.session + "/" + req.file + "'></li>");
 		}
 	}
 
 	function displayNewStopMotion(req){
 		if(req.extension == "mp4"){
-			$('.container-flux .content ul').prepend("<li class='motion-bibli' id='"+ req.title+"'' ><video src='https://localhost:8080/" + app.session + "/" + req.file + "' controls></li>");
+			$('.container-flux .content ul').prepend("<li class='motion-bibli' id='"+ req.title+"'' ><video src='https://"+host+"/" + app.session + "/" + req.file + "' controls></li>");
 		}
 	}
 
 	function displayNewVideo(req){
 		if(req.extension == "webm"){
 			// console.log('prepend content');
-			$('.container-flux .content ul').prepend("<li class='video-bibli' id='"+ req.title+"'' ><video src='https://localhost:8080/" + app.session + "/" + req.file + "' controls></li>");
+			$('.container-flux .content ul').prepend("<li class='video-bibli' id='"+ req.title+"'' ><video src='https://"+host+"/" + app.session + "/" + req.file + "' controls></li>");
 		}
 	}
 
 	function displayNewAudio(req){
 		if(req.extension == "wav"){
-			$('.container-flux .content ul').prepend("<li class='audio-bibli' id='"+ req.title+"'' ><audio src='https://localhost:8080/" + app.session + "/" + req.file + "' controls></li>");
+			$('.container-flux .content ul').prepend("<li class='audio-bibli' id='"+ req.title+"'' ><audio src='https://"+host+"/" + app.session + "/" + req.file + "' controls></li>");
 		}
 	}
 });
